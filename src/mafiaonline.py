7e0b93bf3a60bce5c7b0e68f9385741cd5cce66c
Zakovskiy <gogrugu@gmail.com>
2021-12-06 22:05:44 +0300

    =)

--- src/mafiaonline.py
+++ src/mafiaonline.py
@@ -1,59 +1,116 @@
-import requests, socket, json
-from utils import tags
+import requests
+import socket
+import json
+import threading
 from utils import md5hash
+
 
 class Client:
 
-	def __init__(self, email, password):
-		self.account = None
-		self.token = None
-		self.id = None
-		self.md5hash = md5hash.Client()
-		self.tags = tags.Client()
-		self.create_connection()
-		self.signin("1316eefdd6dc27a9", email, self.md5hash.md5Salt(password))
-		
-	def signin(self, d, e, pw):
-		self.send_server(self.tags.auth_by_email(d, e, pw))
-		self.account = self.listen()["uu"]
-		self.token   = self.account["t"]
-		self.id      = self.account["o"]
-		return self.account
+    def __init__(self, email, password, deviceId: str = "1316eefdd6dc27a9"):
+        self.account = None
+        self.token = None
+        self.id = None
+        self.deviceId = deviceId
+        self.md5hash = md5hash.Client()
+        self.data = []
+        self.create_connection()
+        self.address = "37.143.8.68"
+        self.sign_in(self.deviceId, email, password)
 
-	def friend_list(self, user_id:str = None):
-		self.send_server(self.tags.acfl(self.token, user_id if user_id else self.id))
-		return self.listen()
+    def sign_in(self, deviceId, email, password):
+        data = {
+            "d":  deviceId,
+            "ty":  "sin",
+            "e":  email,
+            "pw":  self.md5hash.md5Salt(password)
+        }
+        self.send_server(data)
+        self.account = self.listen()["uu"]
+        self.token = self.account["t"]
+        self.id = self.account["o"]
+        return self.account
 
-	def get_messages(self, friend_id):
-		self.send_server(self.tags.acpc(self.token, self.id, friend_id))
-		return self.listen()
+    def sign_up(self, nickname, email, password, lang: str = "RUS"):
+        data = {
+            "email":  email,
+            "username":  "",
+            "password":  self.md5hash.md5Salt(password),
+            "deviceId":  self.deviceId,
+            "lang":  lang
+        }
+        res = requests.post(f"http://{self.address}:8008/user/sign_up",
+            data=data,
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        ).json()
+        return res
 
-	def join_global_chat(self):
-		self.send_server(self.tags.acc(self.token, self.id))
+    def friend_list(self):
+        self.send_server({"ty":  "acfl"})
+        return self.listen()
 
-	def send_message_friend(self, friend_id, content):
-		self.send_server(self.tags.ac(friend_id))
-		self.send_server(self.tags.pmc(friend_id, content))
-		return self.listen()
+    def get_messages(self, friend_id):
+        self.send_server({"ty":  "acpc", "fp": friend_id})
+        return self.listen()
 
-	def send_message_global(self, content):
-		self.send_server(self.tags.cmc(content))
+    def join_global_chat(self):
+        self.send_server({"ty": "acc"})
 
-	def get_user(self, user_id):
-		self.send_server(self.tags.gup(user_id))
-		return self.listen()
+    def leave_global_chat(self):
+        self.dashboard()
 
-	def create_connection(self):
-		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.client_socket.connect(('46.175.171.134', 8090))
+    def dashboard(self):
+        self.send_server({"ty": "acd"})
 
-	def send_server(self, msg):
-		msg += "\n"
-		self.client_socket.send(msg.encode())
+    def send_message_friend(self, friend_id, content):
+        self.send_server({"ty": "ac", "fp": friend_id})
+        self.send_server({"ty": "pmc", "m": {"fp": friend_id, "tx": content}})
+        return self.listen()
 
-	def listen(self):
-		result = self.client_socket.recv(1024).decode().replace("\n", "")
+    def send_message_global(self, content):
+        self.send_server({"ty": "cmc", "m": {"tx": content}})
 
-		result = json.loads(result[0:-1])
+    def get_user(self, user_id):
+        self.send_server({"ty": "gup"})
+        return self.listen()
 
-		return result
+    def create_connection(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.address, 8090))
+        self.listener = threading.Thread(target=self.__listener).start()
+
+    def __listener(self):
+        while True:
+            buffer = bytes()
+            while True:
+                r = self.client_socket.recv(2084)
+                read = len(r)
+                if read != -1:
+                    i = read - 1
+                    if r[i] == 0:
+                        buffer = buffer + r
+                        d = buffer.decode()
+                        buffer = bytes()
+                        for str in d.strip().split("[\u0000]"):
+                            str = str.strip()[0:-1]
+                            if str != "p":
+                                print("DEBUG>>"+str)
+                                self.data.append(str)
+                    else:
+                        buffer = buffer + r
+                else:
+                    return
+
+    def send_server(self, j: list):
+        j["t"] = self.token
+        j["uo"] = self.id
+        self.client_socket.send((json.dumps(j)+"\n").encode())
+
+    def listen(self):
+        while len(self.data) <= 0:
+            pass
+        res = self.data[0]
+        del self.data[0]
+        return json.loads(res)
